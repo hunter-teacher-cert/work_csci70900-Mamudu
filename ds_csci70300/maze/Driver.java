11636541704f6796feb530a69da20dc5242854e2
Mamudu <42047513+Mamudu@users.noreply.github.com>
2021-07-13 19:41:35 +0000

    New updates 7/13/2021

--- ds_csci70300/maze/Driver.java
+++ ds_csci70300/maze/Driver.java
@@ -6,9 +6,9 @@ public class Driver{
   public static void main(String[] args) {
     boolean solved;
 
-    // Maze m = new Maze("maze.dat");
-    // System.out.println(m);
-    // solved = m.solve(1,1);
+    Maze m = new Maze("maze.dat");
+    System.out.println(m);
+    solved = m.solve(1,1);
 
 
     Knights k = new Knights(4);
