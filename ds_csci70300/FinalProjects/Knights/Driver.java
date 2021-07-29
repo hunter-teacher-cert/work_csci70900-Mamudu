adfe68169a07e4c2aa550762df65c6adf3c9801a
Mamudu <42047513+Mamudu@users.noreply.github.com>
2021-07-29 16:58:14 +0000

    Knights Completed

---
+++ ds_csci70300/FinalProjects/Knights/Driver.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+
+  public static void main(String[] args) {
+    boolean solved;
+
+    Maze m = new Maze("maze.dat");
+    System.out.println(m);
+    solved = m.solve(1,1);
+
+
+    Knights k = new Knights(4);
+    solved = k.solve(3,2,1); // (1,0) shifted
+
+  }
+}
